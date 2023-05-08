555851096cf73643775e070e7fd47e7a32f56653
Emanuel Vitorino <emanuelvitorino34@gmail.com>
2023-05-09 00:27:07 +0100

    Improvements on the UI

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,14 +1,31 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace CloudMigrationTool;
 
 public partial class MainWindow : Window
 {
-    public bool SourceCloudChosen { get; set; };
+    public bool SourceCloudChosen { get; set; }
+    public bool DestinationCloudChosen { get; set; }
+    public bool IsMigrating { get; set; }
+    public int MigrationProgress { get; set; }
 
     public MainWindow()
     {
         InitializeComponent();
+        IsMigrating = false;
         SourceCloudChosen = false;
+        DestinationCloudChosen = false;
+        MigrationProgress = 0;
+        DataContext = this;
+    }
+    private void StartMigration(object sender, RoutedEventArgs e)
+    {
+        IsMigrating = true;
+    }
+
+    private void CancelMigration(object sender, RoutedEventArgs e)
+    {
+        IsMigrating = false;
     }
 }
